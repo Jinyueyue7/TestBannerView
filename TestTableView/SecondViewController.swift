58fcd0557d50bba756b78d319881c749ae6c0ce3
金悦悦 <921386013@qq.com>
2018-11-02 13:47:11 +0800

    添加自动循环滚动

--- TestTableView/SecondViewController.swift
+++ TestTableView/SecondViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+//    var timer : Timer! = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(autoBanner(_:)), userInfo: nil, repeats: true)
+//
+//    @objc func autoBanner(_ timer: Timer) -> Void {
+//
+//        print("定时器")
+//
+//        let offsetX = collectionView.contentOffset.x
+//
+//        let index = offsetX / view.frame.width
+//
+//        if index ==  CGFloat(dataArray.count - 2) {
+//
+//            collectionView.contentOffset.x = view.frame.width
+//
+//
+//        }else {
+//            let currentOffsetX = offsetX + view.frame.width
+//            collectionView.contentOffset.x = currentOffsetX
+//        }
+//    }
+    
     private lazy var collectionView : UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
@@ -23,9 +44,24 @@ class SecondViewController: UIViewController {
         collectionView.isPagingEnabled = true
         collectionView.delegate = self
         collectionView.dataSource = self
+        //是否需要弹簧效果
+        collectionView.bounces = false
         collectionView.register(BannerCollectionViewCell.self , forCellWithReuseIdentifier: TestTableViewCell.identifier())
         
         return collectionView
+    }()
+    
+    private lazy var pageControl : UIPageControl = {
+        let pageControl = UIPageControl(frame: CGRect(x: 0, y: 170, width: view.frame.width, height: 30))
+        
+        pageControl.currentPage = 0
+        
+        pageControl.numberOfPages = 3
+        
+        pageControl.pageIndicatorTintColor = .blue
+        pageControl.currentPageIndicatorTintColor = .orange
+        
+        return pageControl
     }()
     
     private lazy var dataArray : [String] = ["tu3","tu1","tu2","tu3","tu1"]
@@ -33,48 +69,89 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
         view.backgroundColor = UIColor.cyan
+        self.edgesForExtendedLayout = []
         
         view.addSubview(collectionView)
+        view.addSubview(pageControl)
         
+        //设置初始图片
+        let indexPath = IndexPath(item: 1, section: 0) as NSIndexPath
+        collectionView.scrollToItem(at: indexPath as IndexPath, at: UICollectionView.ScrollPosition.centeredHorizontally, animated: false)
+        
+        //设置定时器
+        setTimer()
     }
 }
 
 extension SecondViewController : UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        
+
         return dataArray.count
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TestTableViewCell.identifier(), for: indexPath) as! BannerCollectionViewCell
-        
-        
+
         cell.imageName = dataArray[indexPath.row]
         
         return cell
-        
     }
     
+    //滑动图片切换
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
         let offsetX = scrollView.contentOffset.x
         
         let index = offsetX / view.frame.width
-        
+
+        var currentIndex = 0
+
+        //第一页往前滑跳转到最后一页
         if index == 0 {
             let indexPath = IndexPath(item: dataArray.count - 2, section: 0) as NSIndexPath
             
-            collectionView.scrollToItem(at: indexPath as IndexPath, at: UICollectionView.ScrollPosition.centeredHorizontally, animated: false)
-        }
-        
-        if index == CGFloat(dataArray.count - 1) {
-            let indexPath = IndexPath(item: 1, section: 0) as NSIndexPath
+            currentIndex = dataArray.count - 2
             
             collectionView.scrollToItem(at: indexPath as IndexPath, at: UICollectionView.ScrollPosition.centeredHorizontally, animated: false)
+        }else if index == CGFloat(dataArray.count - 1) {
+            //最后一页往后滑跳转到第一页
+            let indexPath = IndexPath(item: 1, section: 0) as NSIndexPath
+            
+            currentIndex = 0
+            
+            collectionView.scrollToItem(at: indexPath as IndexPath, at: UICollectionView.ScrollPosition.centeredHorizontally, animated: false)
+        }else {
+            
+            print(index)
+            print(round(index))
+            print("-----")
+
+            let roundIndex = round(index)
+            
+               currentIndex = Int(roundIndex) - 1
+        }
+        pageControl.currentPage = currentIndex
+    }
+}
+
+extension SecondViewController {
+    
+    func setTimer() {
+        _ = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
+            let offsetX = self.collectionView.contentOffset.x
+            
+            let index = offsetX / self.view.frame.width
+            
+            if index ==  CGFloat(self.dataArray.count - 2) {
+                
+                self.collectionView.contentOffset.x = self.view.frame.width
+                
+                
+            }else {
+                let currentOffsetX = offsetX + self.view.frame.width
+                self.collectionView.contentOffset.x = currentOffsetX
+            }
         }
     }
     
